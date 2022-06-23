3d955a013dc2eaa8d2d49956e36ade0918d96e3a
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-06-23 07:29:40 -0700

    Clean up spi_unit.rs

--- tests/spi_unit.rs
+++ tests/spi_unit.rs
@@ -1,16 +1,15 @@
 #[cfg(test)]
-mod tests {
-    use atwinc1500::error;
+mod spi_unit_tests {
     use atwinc1500::spi::SpiBusWrapper;
     use std::io::ErrorKind;
-    //use embedded_hal::digital::v2::OutputPin;
-    //use embedded_hal::spi::FullDuplex;
     use embedded_hal_mock::pin::{
         Mock as PinMock, State as PinState, Transaction as PinTransaction,
     };
     use embedded_hal_mock::spi::{Mock as SpiMock, Transaction as SpiTransaction};
     use embedded_hal_mock::MockError;
 
+    /// Returns an SpiBusWrapper with
+    /// mocked spi and mocked chip select
     fn get_fixture(
         spi_expect: &[SpiTransaction],
         pin_expect: &[PinTransaction],
@@ -26,10 +25,7 @@ mod tests {
         let spi_expect = [];
         let pin_expect = [PinTransaction::set(PinState::High).with_error(err.clone())];
         let mut spi_bus = get_fixture(&spi_expect, &pin_expect);
-        match spi_bus.init_cs() {
-            Ok(()) => {}
-            Err(e) => assert_eq!(e, error::Error::PinStateError),
-        }
+        assert!(spi_bus.init_cs().is_err());
     }
 
     #[test]
@@ -39,10 +35,4 @@ mod tests {
         let mut spi_bus = get_fixture(&spi_expect, &pin_expect);
         assert!(spi_bus.init_cs().is_ok());
     }
-
-    //#[test]
-    //fn test_read_register() {
-    //    let spi_expect = [];
-    //    let spi_bus = get_fixture(&spi_expect);
-    //}
 }
